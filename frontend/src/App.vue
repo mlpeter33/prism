6801cbaecf578801e15d6c2d1eb3e7821f24e3c2
mlpeter33 <mlacayopit@gmail.com>
2025-03-13 22:55:45 +0000

    tweaks

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,7 +4,16 @@ import { ref } from 'vue';
 const message = ref('Welcome to Prism! 🚀');
 </script>
 <template>
-    <div class="container bg-white h-screen w-screen">
+    <div class="container bg-white min-h-full min-w-full">
       <h1 class=" text-black">{{ message }}</h1>
     </div>
   </template>
+  <style>
+  html, body, #app {
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    }
+  </style>
