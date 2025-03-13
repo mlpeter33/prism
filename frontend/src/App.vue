fdad7ca25460268a48d3a65f718ef76b4fae65b2
mlpeter33 <mlacayopit@gmail.com>
2025-03-13 20:30:14 +0000

    vue instegration

---
+++ frontend/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue';
+
+const message = ref('Welcome to Prism! 🚀');
+</script>
+<template>
+    <div class="container bg-white h-screen w-screen">
+      <h1 class=" text-black">{{ message }}</h1>
+    </div>
+  </template>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
-    tailwindcss(),
+    tailwindcss(), vue()
   ],
+  server: {
+    strictPort: true,
+  },
+  clearScreen: false,
 })
